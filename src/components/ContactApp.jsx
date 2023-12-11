3d59645384b6007454e6d254ab59ae114348af37
sepriandani <sepriandanni@gmail.com>
2023-12-11 17:07:54 +0700

    menerapkan componenet lifecycle

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -14,9 +14,11 @@ class ContactApp extends React.Component {
 
     this.state = {
       authedUser: null,
+      initializing: true,
     }
 
     this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    this.onLogout = this.onLogout.bind(this);
   }
 
   async onLoginSuccess({ accessToken }) {
@@ -35,18 +37,31 @@ class ContactApp extends React.Component {
     this.setState(() => {
       return {
         authedUser: data,
+        initializing: false,
       };
     });
   }
 
+  onLogout() {
+    this.setState(() => {
+      return{
+        authedUser: null,
+      }
+    });
+    putAccessToken("");
+  }
+
   render() {
+
+    if(this.state.initializing){
+      return null;
+    }
 
     if(this.state.authedUser === null) {
       return(
         <div className="contact-app">
           <header className="contact-app__header">
             <h1>Aplikasi Kontak</h1>
-            <Navigation />
           </header>
           <main>
             <Routes>
@@ -62,7 +77,7 @@ class ContactApp extends React.Component {
       <div className="contact-app">
         <header className="contact-app__header">
           <h1>Aplikasi Kontak</h1>
-          <Navigation />
+          <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
         </header>
         <main>
           <Routes>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import PropsTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { FiHome, FiPlusCircle } from "react-icons/fi";
+import { FiHome, FiLogOut, FiPlusCircle } from "react-icons/fi";
 
-function Navigation() {
+function Navigation({ logout, name }) {
     return(
         <nav className="navigation">
             <ul>
                 <li><Link to="/" ><FiHome /></Link></li>
                 <li><Link to="/add" ><FiPlusCircle /></Link></li>
+                <li><button onClick={logout} >{ name } <FiLogOut /></button></li>
             </ul>
         </nav>
     )
+}
+
+Navigation.propsTypes = {
+    logout: PropsTypes.func.isRequired,
+    name: PropsTypes.string.isRequired,
 }
 
 export default Navigation;
